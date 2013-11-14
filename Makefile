ad8cf3fa2d1230bdbdb4c1fd0503ef14e353e9c0
Atsuki Demizu <demmy@sfc.wide.ad.jp>
2013-11-14 10:18:32 +0900

    fixed: embody all device program to brainfuck.*

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
 NVCC = nvcc
-CFLAGS = -gencode=arch=compute_20,code=\"sm_20,compute_20\" -rdc=true
-LDFLAGS = -lcudart
+CFLAGS = -gencode=arch=compute_20,code=\"sm_20,compute_20\"
 
 TARGET = gpubf
-OBJS = util.o transmit.o compile.o run.o brainfuck.o main.o
+OBJS = util.o transmit.o brainfuck.o main.o
 
 .SUFFIXES: .cu .o
 .PHONY: all run clean clear
@@ -14,7 +13,7 @@ run: $(TARGET)
 	./$(TARGET)
 
 $(TARGET): $(OBJS)
-	$(NVCC) $(CFLAGS) $(LDFLAGS) -o $(TARGET) $(OBJS)
+	$(NVCC) -o $(TARGET) $(OBJS)
 
 .cu.o:
 	$(NVCC) $(CFLAGS) -c $<
--- brainfuck.h
+++ brainfuck.h
@@ -1,11 +1,51 @@
 #ifndef BRAINFUCK_H_INCLUDED
 #define BRAINFUCK_H_INCLUDED
-#include "run.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Enumerated type
+ */
+typedef enum{
+    INC,
+    DEC,
+    NEXT,
+    PREV,
+    GET,
+    PUT,
+    BEGIN,
+    END,
+    EOP
+} Token;
+
+typedef enum{
+    ADD_EXPRESSION,
+    MOVE_EXPRESSION,
+    GET_EXPRESSION,
+    PUT_EXPRESSION,
+    WHILE_EXPRESSION
+} ExpressionKind;
+
+/*
+ * Structure
+ */
+struct Expression_tag{
+    ExpressionKind kind;
+    union{
+        int value;
+        Expression_tag *block;
+    } u;
+    Expression_tag *next;
+    Expression_tag *prev;
+};
+typedef struct Expression_tag Expression;
 
 /*
  * Function prototype
  */
 __global__ void kernel(char *res, char *data);
 __device__ char brainfuck(char *source, int len);
+__device__ Expression *parse(char **source, Token period);
+__device__ char run(Expression *program);
 
 #endif // BRAINFUCK_H_INCLUDED
