66c8ea0c8a8563f5dde08e34fe7a469f5d7df397
Atsuki Demizu <demmy@sfc.wide.ad.jp>
2013-11-18 02:40:55 +0900

    added: digit flag help

--- print.cu
+++ print.cu
@@ -15,6 +15,7 @@ __host__ void help(void){
     puts(usage);
     puts("\noptions:");
     puts("\t-c\tRun same program on 1 core 1 thread CPU.");
+    puts("\t-d\tDisplay result of execution in digit.");
     puts("\t-h\tDisplay available options and exit.");
     puts("\t-m\tWhen use with -t option, display real run time includes memory copy time between host and device after the execution.");
     puts("\t-t\tDisplay real run time after the execution.");
