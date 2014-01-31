06568273ca6d66ac590d0fc9386d5afc5a4312db
Atsuki Demizu <demmys.atsuki@gmail.com>
2014-01-31 17:39:16 +0900

    added: tick1 speed time

--- gnuplot/measure_speed.sh
+++ gnuplot/measure_speed.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 
 echo "processor $processor start."
-dir=${processor}_speed
+dir=${processor}_speed_tick1
 mkdir $dir
-average="${dir}/average.log"
+average="${dir}/average_tick1.log"
 touch $average
 
 bf=tester.bf
-cp 1000000.bf $bf
-vim -c 'normal ggyy15p' -c 'wq' $bf
+cp 500000.bf $bf
 
-for i in `seq 1 64`
+for i in `seq 1 256`
 do
-    thread=`expr 16 \* $i`
+    thread=$i
     file="${dir}/${thread}thread.log"
     touch $file
     echo "processing ${file}..."
     for j in `seq 1 10`
     do
+        echo -n "${j}"
         if test "$processor" = "cpu"
         then
             ./gpubf -ctl $bf >> $file
         else
             ./gpubf -tl $bf >> $file
         fi
+        if test $j = 10
+        then
+            echo
+        else
+            echo -n ", "
+        fi
     done
     cat $file
     node make_average.js $file >> $average
-    vim -c 'normal ggyy16p' -c 'wq' $bf
+    vim -c 'normal ggyyp' -c 'wq' $bf
 done
 
 rm $bf
